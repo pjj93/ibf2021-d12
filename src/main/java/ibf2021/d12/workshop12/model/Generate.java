35207e2a105c5df906c5923ab9555c360c6e0c34
Mac <mac@Macs-MacBook-Pro.local>
2022-01-07 10:59:25 +0800

    added comments

--- src/main/java/ibf2021/d12/workshop12/model/Generate.java
+++ src/main/java/ibf2021/d12/workshop12/model/Generate.java
@@ -3,6 +3,9 @@ package ibf2021.d12.workshop12.model;
 import java.io.Serializable;
 
 public class Generate implements Serializable{
+    // serialization is writing and saving a file
+    // ensures that jdk can identify the object when it is streamed
+    // Serialization should be implmented for Java objects that is streamed through a network
     private int numberVal;
     
     public void setNumberVal(int numberVal) {
